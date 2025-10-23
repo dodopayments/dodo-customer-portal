c300327ea9786e40b47ed4b80e27028f45be8dd0
Sahil Tiwaskar <sahiltiwaskart@gmail.com>
2025-10-23 14:03:49 +0530

    rm redux 2

--- src/app/session/subscriptions/subscriptions-table.tsx
+++ src/app/session/subscriptions/subscriptions-table.tsx
@@ -4,6 +4,7 @@ import BaseDataTable from "@/components/custom/base-data-table";
 import { createSubscriptionColumns } from "@/components/session/subscription-column";
 
 interface SubscriptionsTableProps {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   data: any[];
 }
 
