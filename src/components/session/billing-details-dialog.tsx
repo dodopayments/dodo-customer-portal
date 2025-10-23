eb2340c39a622e44db7203af8797be727e7d73d6
Sahil Tiwaskar <sahiltiwaskart@gmail.com>
2025-10-23 12:27:00 +0530

    rm redux 1

--- src/components/session/billing-details-dialog.tsx
+++ src/components/session/billing-details-dialog.tsx
@@ -30,7 +30,39 @@ import {
   billingDetailsFormSchema,
   BillingDetailsFormValues,
 } from "./subscription-form-schema";
-import { SubscriptionResponse } from "@/redux/slice/subscription/subscriptoinSlice";
+interface SubscriptionResponse {
+  created_at: string;
+  currency: string;
+  customer: {
+    customer_id: string;
+    email: string;
+    name: string;
+  };
+  billing: {
+    city: string;
+    country: string;
+    state: string;
+    street: string;
+    zipcode: string;
+  };
+  discount_id: string;
+  metadata: object;
+  next_billing_date: string;
+  on_demand: boolean;
+  payment_frequency_count: number;
+  payment_frequency_interval: string;
+  product_id: string;
+  quantity: number;
+  recurring_pre_tax_amount: number;
+  status: string;
+  subscription_id: string;
+  subscription_period_count: number;
+  subscription_period_interval: string;
+  tax_inclusive: boolean;
+  tax_id?: string;
+  trial_period_days: number;
+  cancel_at_next_billing_date?: boolean;
+}
 
 interface BillingDetailsDialogProps {
   open: boolean;
