ee21343a36ae0bfdd14bf98088add121e95eae46
Sahil Tiwaskar <sahiltiwaskart@gmail.com>
2025-10-27 14:23:54 +0530

    refactor(login): update layout and login card structure, enhance responsiveness, and modify footer behavior

--- src/app/login/[business_id]/layout.tsx
+++ src/app/login/[business_id]/layout.tsx
@@ -3,7 +3,7 @@ import Background from "@/components/login/background";
 const Loginlayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="relative min-h-screen w-full">
-      <Background />
+      {/* <Background /> */}
       {children}
     </main>
   );
