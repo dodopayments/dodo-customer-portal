66a36ed4c94d70b70ff227f4958fa4ea184e22f4
Sahil Tiwaskar <sahiltiwaskart@gmail.com>
2025-10-27 16:23:34 +0530

    add default structure

--- src/app/session/layout.tsx
+++ src/app/session/layout.tsx
@@ -24,8 +24,8 @@ const Dashboardlayout = ({ children }: { children: React.ReactNode }) => {
       />
       <AppSidebar />
       {/* <Navbar /> */}
-      <main className="flex-1 overflow-y-auto">
-        <div className="h-full">
+      <main className="flex-1 overflow-y-auto pt-6 pl-3">
+        <div className="h-full border-l border-t border-border-secondary rounded-tl-2xl shadow-[-3px_-3px_20px_0px_var(--bg-secondary)]">
           {children}
         </div>
       </main>
