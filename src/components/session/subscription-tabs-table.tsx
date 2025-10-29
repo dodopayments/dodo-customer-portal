ff2b9a043598c7620665c9582c489a244e142b5f
Sahil Tiwaskar <sahiltiwaskart@gmail.com>
2025-10-29 15:07:48 +0530

    feat(usage): add usage history fetching and display in subscription tabs, implement usage summary component for enhanced data visualization

--- src/components/session/subscription-tabs-table.tsx
+++ src/components/session/subscription-tabs-table.tsx
@@ -2,8 +2,9 @@ import { Separator } from "../ui/separator";
 import { SessionTabs } from "./tabs";
 import { SubscriptionResponse } from "@/types/subscription";
 import { InvoiceHistory } from "./invoice-history";
-import { fetchInvoiceHistory } from "@/app/session/subscriptions/[id]/action";
+import { fetchInvoiceHistory, fetchUsageHistory } from "@/app/session/subscriptions/[id]/action";
 import { api_url } from "@/lib/http";
+import { UsageSummary } from "./usage-summary";
 
 export interface InvoiceHistoryResponse {
     payment_id: string;
@@ -14,6 +15,24 @@ export interface InvoiceHistoryResponse {
     status: string;
     digital_products_delivered: boolean;
     download_url: string;
+}
+
+export interface UsageHistoryMeter {
+    id: string;
+    date: string; // date
+    name: string; // meter name
+    free_threshold: number;  // threshold units
+    price_per_unit: string; // unit price
+    chargeable_units: string; // fixed fee
+    consumed_units: string; // consumed units
+    currency: string; // currency
+    total_price: number; // total price
+}
+
+export interface UsageHistoryResponse {
+    end_date: string;
+    meters: UsageHistoryMeter[];
+    start_date: string;
 }
 
 export async function SubscriptionTabsTable({
@@ -28,6 +47,8 @@ export async function SubscriptionTabsTable({
     const params = await searchParams;
     const tab = params?.tab || 'invoice-history';
     const invoiceHistory = await fetchInvoiceHistory(subscriptionId);
+    const usageHistory = await fetchUsageHistory(subscriptionId);
+
     const invoiceHistoryData: InvoiceHistoryResponse[] = invoiceHistory?.items?.map((item: InvoiceHistoryResponse) => ({
         payment_id: item.payment_id,
         subscription_id: subscriptionId,
@@ -38,6 +59,23 @@ export async function SubscriptionTabsTable({
         digital_products_delivered: item.digital_products_delivered,
         download_url: `${api_url}/invoices/payments/${item.payment_id}`,
     }));
+
+    const usageHistoryData: UsageHistoryResponse[] = usageHistory?.items?.map((item: UsageHistoryResponse) => ({
+        start_date: item.start_date,
+        end_date: item.end_date,
+        meters: item.meters.map((meter: UsageHistoryMeter) => ({
+            date: item.start_date,
+            chargeable_units: meter.chargeable_units,
+            consumed_units: meter.consumed_units,
+            currency: meter.currency,
+            free_threshold: meter.free_threshold,
+            id: meter.id,
+            name: meter.name,
+            price_per_unit: meter.price_per_unit,
+            total_price: meter.total_price,
+        })),
+    }));
+
     return (
         <div className="flex flex-col gap-8">
             <div className="flex flex-col">
@@ -57,6 +95,7 @@ export async function SubscriptionTabsTable({
                 <Separator className="my-0" />
             </div>
             {tab === 'invoice-history' && <InvoiceHistory invoiceHistory={invoiceHistoryData} />}
+            {tab === 'usage-summary' && <UsageSummary usageHistory={usageHistoryData} />}
         </div>
     )
 }
