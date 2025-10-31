5d9f01ec3493dc58fd5ee00dcbf38d547387ebcd
Sahil Tiwaskar <sahiltiwaskart@gmail.com>
2025-10-31 17:28:06 +0530

    feat(subscriptions): update AddOn interface, enhance SubscriptionDetails component with AddOns display, and introduce parseIsoDateDMY for date formatting

--- src/components/session/subscription-details.tsx
+++ src/components/session/subscription-details.tsx
@@ -2,9 +2,10 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card";
 import { Badge } from "../ui/badge";
 import { Separator } from "../ui/separator";
-import { parseIsoDate } from "@/lib/date-helper";
+import { parseIsoDateDMY } from "@/lib/date-helper";
 import { CurrencyCode, formatCurrency, decodeCurrency } from "@/lib/currency-helper";
-import { SubscriptionDetailsData } from "@/app/session/subscriptions/[id]/types";
+import { AddOn, SubscriptionDetailsData } from "@/app/session/subscriptions/[id]/types";
+import { Button } from "../ui/button";
 
 export function SubscriptionDetails({ subscription }: { subscription: SubscriptionDetailsData }) {
     console.log("subscription", subscription);
@@ -17,13 +18,16 @@ export function SubscriptionDetails({ subscription }: { subscription: Subscripti
                         <CardTitle>{formatCurrency(decodeCurrency(subscription.recurring_pre_tax_amount, subscription.currency as CurrencyCode), subscription.currency as CurrencyCode)}/{subscription.payment_frequency_interval.toLowerCase()}</CardTitle>
                         <CardDescription className="text-text-secondary text-sm">{subscription.product.description}</CardDescription>
                     </CardHeader>
-                    <CardContent className="flex flex-row gap-2 p-0">
-                        <Badge variant="default" dot={false} className="rounded-sm border-sm">
-                            renews on {parseIsoDate(subscription.next_billing_date)}
-                        </Badge>
-                        <Badge variant="default" dot={false} className="rounded-sm border-sm">
-                            valid until {parseIsoDate(subscription.next_billing_date)}
-                        </Badge>
+                    <CardContent className="flex flex-col gap-4 p-0">
+                        <div className="flex flex-row gap-2">
+                            <Badge variant="default" dot={false} className="rounded-sm border-sm">
+                                renews on {parseIsoDateDMY(subscription.next_billing_date)}
+                            </Badge>
+                            <Badge variant="default" dot={false} className="rounded-sm border-sm">
+                                valid until {parseIsoDateDMY(subscription.next_billing_date)}
+                            </Badge>
+                        </div>
+                        <AddOns addons={subscription.addons} />
                     </CardContent>
 
                 </Card>
@@ -38,4 +42,37 @@ export function SubscriptionDetails({ subscription }: { subscription: Subscripti
         </div>
 
     );
+}
+
+function AddOns({ addons }: { addons: AddOn[] }) {
+    return (
+        <Card className="p-6 flex flex-col gap-4">
+            <CardHeader className="flex flex-row gap-2 p-0 justify-between">
+                <CardTitle
+                    className="font-['Hanken_Grotesk'] font-medium my-auto text-md leading-5 tracking-normal"
+                    style={{ leadingTrim: 'cap-height' } as React.CSSProperties}
+                >
+                    Addons
+                </CardTitle>
+                <Button variant="secondary" className="w-fit my-auto">Update</Button>
+            </CardHeader>
+            <Separator />
+            <CardContent className="flex flex-col gap-2 p-0">
+                {addons.map((addon) => (
+                    <Card key={addon.addon_id} className="p-4 flex flex-col gap-2">
+                        <div className="flex flex-row gap-2">
+                            <CardTitle className="font-['Hanken_Grotesk'] font-medium my-auto text-md leading-5 tracking-normal">
+                                {addon.addon_id}
+                            </CardTitle>
+                        </div>
+                        <div className="flex flex-row gap-2">
+                            <CardDescription className="font-['Inter'] font-normal text-sm leading-[21px] text-text-secondary self-stretch">
+                                {addon.quantity}
+                            </CardDescription>
+                        </div>
+                    </Card>
+                ))}
+            </CardContent>
+        </Card>
+    )
 }
