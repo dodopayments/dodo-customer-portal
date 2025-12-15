c373568186af2c63fe0b3322a3a830d5c3bc009e
Sahil Tiwaskar <sahiltiwaskart@gmail.com>
2025-12-15 19:28:46 +0530

    Enhance landing and businesses pages with business token retrieval and search functionality; add BusinessCard and Businesses components for displaying business data

--- src/app/businesses/page.tsx
+++ src/app/businesses/page.tsx
@@ -1,6 +1,7 @@
 import { extractPaginationParams } from "@/lib/pagination-utils";
 import { fetchBusinesses } from "./action";
 import NavbarTwo from "@/components/navbar-two";
+import { Businesses } from "@/components/businesses/businesses";
 
 interface BusinessesPageProps {
   searchParams: Promise<Record<string, string | string[] | undefined>>;
@@ -23,6 +24,18 @@ export default async function BusinessesPage({
   return (
     <div className="w-full px-4 md:px-12 py-4 md:py-6 mb-16 flex flex-col h-ful container mx-auto">
       <NavbarTwo />
+
+      <div className="w-full flex flex-col gap-4 mt-4">
+      <Businesses
+        businessData={businessesData.data}
+        currentPage={currentPage}
+        pageSize={pageSize}
+        currentPageItems={businessesData.data.length}
+        hasNextPage={businessesData.hasNext}
+        baseUrl={baseUrl}
+        pageParamKey={PAGE_PARAM_KEY}
+        />
+        </div>
     </div>
   );
 }
---
+++ src/components/businesses/businesses.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import { RefreshCcw } from "lucide-react";
+import ServerPagination from "@/components/common/server-pagination";
+import { BusinessCard } from "./business-card";
+import { Input } from "../ui/input";
+import { useState } from "react";
+
+export interface BusinessData {
+  business_id: string;
+  contry: string;
+  logo: string;
+  name: string;
+}
+
+interface ItemCardProps {
+  businessData: BusinessData[];
+  dataIndex?: number;
+  currentPage: number;
+  pageSize: number;
+  currentPageItems: number;
+  hasNextPage: boolean;
+  baseUrl: string;
+  pageParamKey?: string;
+}
+
+export const Businesses = ({
+  businessData,
+  currentPage,
+  pageSize,
+  currentPageItems,
+  hasNextPage,
+  baseUrl,
+  pageParamKey,
+}: ItemCardProps) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const isEmpty = businessData.length === 0;
+  const emptyMessage =
+    currentPage > 0
+      ? "No businesses found on this page"
+      : "No active businesses at the moment";
+
+  return (
+    <div className="flex flex-col gap-4 mt-4">
+      <div className="flex flex-col gap-4 mb-4">
+        <Input
+          type="text"
+          placeholder="Search businesses"
+          className="w-full"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+      {isEmpty ? (
+        <div className="flex flex-col justify-center items-center min-h-[calc(100vh-20rem)] my-auto">
+          <span className="text-text-primary p-3 mb-3 bg-bg-secondary rounded-full text-2xl">
+            <RefreshCcw />
+          </span>
+          <span className="text-sm font-display text-center tracking-wide text-text-secondary">
+            {emptyMessage}
+          </span>
+        </div>
+      ) : (
+        businessData
+          .filter((item: BusinessData) =>
+            item.name.toLowerCase().includes(searchTerm.toLowerCase())
+          )
+          .map((item: BusinessData, index: number) => (
+            <BusinessCard key={index} business={item} />
+          ))
+      )}
+      {(!isEmpty || currentPage !== 0 || pageSize !== 50) && (
+        <ServerPagination
+          currentPage={currentPage}
+          pageSize={pageSize}
+          currentPageItems={currentPageItems}
+          hasNextPage={hasNextPage}
+          baseUrl={baseUrl}
+          pageParamKey={pageParamKey}
+        />
+      )}
+    </div>
+  );
+};
