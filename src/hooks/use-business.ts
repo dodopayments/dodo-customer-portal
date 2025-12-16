0de0ad0d788210272a6a5ae9c0041b1286e7778f
Sahil Tiwaskar <sahiltiwaskart@gmail.com>
2025-12-16 12:33:47 +0530

    Update useBusiness hook to depend on pathname for loading business data

--- src/hooks/use-business.ts
+++ src/hooks/use-business.ts
@@ -3,12 +3,14 @@
 import { useState, useEffect } from "react";
 import { fetchBusiness } from "@/lib/server-actions";
 import parseError from "@/lib/clientErrorHelper";
+import { usePathname } from "next/navigation";
 
 export function useBusiness() {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const [business, setBusiness] = useState<any>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+  const pathname = usePathname();
 
   useEffect(() => {
     const loadBusiness = async () => {
@@ -26,7 +28,7 @@ export function useBusiness() {
     };
 
     loadBusiness();
-  }, []);
+  }, [pathname]);
 
   return { business, loading, error };
 }
